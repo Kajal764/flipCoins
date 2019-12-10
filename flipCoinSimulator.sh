933384576adc980bfc22f2b8767931fb47f0068d
kajal764 <kajaldw666@gmail.com>
2019-12-10 16:44:05 +0530

    Added: flip 1 coin and calculate percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,49 @@
 echo "Welcome to FlipCoinSimulator"
-H=0
-T=1
-coin=$(RANDOM%2)
 
-if [[ $coin -eq 0 ]]
-then
-	echo "head"
-else
-	echo "tail"
-fi 
+declare -A coin
+HEAD=H
+TAIL=T
+SINGLET=1
 
+function getRandomFlip(){
+   value=$((RANDOM%2))
+	echo $value
+}
+
+function getFlipCoin(){
+   numOfFlip=1
+   for(( j=1;j<=numOfFlip;j++ ))
+   do
+      value=$(($(getRandomFlip)))
+   done
+	echo $value
+}
+
+function main(){
+	flip=$1
+	countT=0
+	countH=0
+	for (( i=1;i<=10;i++ ))
+	do
+		value=$(getFlipCoin)
+		if [[ $value -eq 0 ]]
+		then
+			key="H"
+			countH=$(($countH+1))
+			coin[$key]="T---> $countT"
+
+		else
+			key="T"
+			countT=$(($countT+1))
+			coin[$key]="H---> $countH"
+
+		fi
+	done
+
+echo "H=$(($(($countH * 100 ))/10))
+echo "T=$(($(($countT * 100 ))/10))
+}
+
+
+
+main $SINGLET
